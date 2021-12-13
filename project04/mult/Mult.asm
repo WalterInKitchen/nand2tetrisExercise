ff3367dd31ba7d5755d5bcc11ba2e7aad1e0a858
Walter <WalterInKitchen@outlook.com>
2021-12-13 22:28:24 +0800

    impl Mult.asm

--- project04/mult/Mult.asm
+++ project04/mult/Mult.asm
@@ -1,2 +1,36 @@
 // Computes result=R0*R1
 
+@i
+M=0
+@R2
+M=0
+
+@R0
+D=M
+@end
+D;JEQ
+
+@R1
+D=M
+@end
+D;JEQ
+
+(loop)
+@R1
+D=M
+@R2
+M=D+M           //R2=R2+R1
+
+@i
+M=M+1           //i=i+1
+
+@R0
+D=M
+@i
+D=D-M
+@loop
+D;JNE           //if i<R0,continue
+
+(end)
+@end
+0;JMP
