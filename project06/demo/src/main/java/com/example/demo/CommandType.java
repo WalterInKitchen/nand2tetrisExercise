75b6acb037a4290682cf5c5e08698a4fa7269a66
walter <WalterInKitchen@outlook.com>
2021-12-29 22:31:57 +0800

    impl the parser and pass the test cases

--- project06/demo/src/main/java/com/example/demo/CommandType.java
+++ project06/demo/src/main/java/com/example/demo/CommandType.java
@@ -1,5 +1,13 @@
 package com.example.demo;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Locale;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public enum CommandType {
     ADDRESS_CMD() {
         @Override
@@ -13,25 +21,89 @@ public enum CommandType {
         }
     },
     CONTROL_CMD() {
+        private final Pattern patternWithoutJump = Pattern.compile("(.*?)=(.*?)");
+        private final Pattern patternWithoutDest = Pattern.compile("(.*?);(.*?)");
+        private final Pattern pattern = Pattern.compile("(.*?)=(.*?);(.*?)");
+        private final Pattern patternFocusDest = Pattern.compile(".*?=.*?");
+        private final Set<String> destSet = new HashSet<>(
+                Arrays.asList("M", "D", "MD", "A", "AM", "AD", "AMD")
+        );
+        private final Set<String> jumpSet = new HashSet<>(
+                Arrays.asList("JGT", "JEQ", "JGE", "JLT", "JNE", "JLE", "JMP")
+        );
+
         @Override
         public boolean match(String cmd) {
-            return false;
+            if (pattern.matcher(cmd).matches()) {
+                return true;
+            }
+            if (patternWithoutDest.matcher(cmd).matches()) {
+                return true;
+            }
+            return patternWithoutJump.matcher(cmd).matches();
         }
 
         @Override
         public String parseDest(String cmd) {
-            return "";
+            if (!patternFocusDest.matcher(cmd).matches()) {
+                return "null";
+            }
+            String dest = cmd.substring(0, cmd.indexOf("="));
+            if (destSet.contains(dest)) {
+                return dest.toUpperCase(Locale.US);
+            }
+            return "null";
+        }
+
+        @Override
+        public String parseComp(String cmd) {
+            Matcher matcher = pattern.matcher(cmd);
+            if (matcher.matches()) {
+                return matcher.group(2);
+            }
+            Matcher matcherWithoutDest = patternWithoutDest.matcher(cmd);
+            if (matcherWithoutDest.matches()) {
+                return matcherWithoutDest.group(1);
+            }
+            Matcher matcherWithoutJump = patternWithoutJump.matcher(cmd);
+            if (matcherWithoutJump.matches()) {
+                return matcherWithoutJump.group(2);
+            }
+            throw new RuntimeException("there's no comp part in cmd:" + cmd);
+        }
+
+        @Override
+        public String parseJump(String cmd) {
+            Matcher matcher = pattern.matcher(cmd);
+            if (matcher.matches()) {
+                return convertJumToSymbol(matcher.group(3));
+            }
+            Matcher matcherWithoutDest = patternWithoutDest.matcher(cmd);
+            if (matcherWithoutDest.matches()) {
+                return convertJumToSymbol(matcherWithoutDest.group(2));
+            }
+            return "null";
+        }
+
+        private String convertJumToSymbol(String symbol) {
+            String upSym = symbol.toUpperCase(Locale.US);
+            if (jumpSet.contains(upSym)) {
+                return upSym;
+            }
+            return "null";
         }
     },
     LOCATION_CMD() {
+        private final Pattern pattern = Pattern.compile("\\(.*\\)");
+
         @Override
         public boolean match(String cmd) {
-            return false;
+            return pattern.matcher(cmd).matches();
         }
 
         @Override
         public String parseSymbol(String cmd) {
-            return "";
+            return cmd.substring(1, cmd.length() - 1);
         }
     };
 
@@ -46,10 +118,10 @@ public enum CommandType {
     }
 
     public String parseComp(String cmd) {
-        return null;
+        throw new RuntimeException("this kind of cmd can not parse comp");
     }
 
     public String parseJump(String cmd) {
-        return null;
+        throw new RuntimeException("this kind of cmd can not parse jump");
     }
 }
--- project06/demo/src/test/java/com/example/demo/ParserImplTest.java
+++ project06/demo/src/test/java/com/example/demo/ParserImplTest.java
@@ -4,6 +4,11 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import java.io.ByteArrayInputStream;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class ParserImplTest {
     /**
@@ -26,5 +31,218 @@ public class ParserImplTest {
         ByteArrayInputStream ins = new ByteArrayInputStream("".getBytes());
         ParserImpl parser = new ParserImpl(ins);
         Assert.assertFalse(parser.hasMoreCommands());
+    }
+
+    /**
+     * @provided the init state
+     * @expected exception
+     **/
+    @Test(expected = RuntimeException.class)
+    public void test_commandType_given_initState_when_getCommandType_then_throwException() {
+        ByteArrayInputStream ins = new ByteArrayInputStream("command".getBytes());
+        ParserImpl parser = new ParserImpl(ins);
+        parser.commandType();
+    }
+
+    /**
+     * @provided the address cmd and advance invoked
+     * @expected the address cmd type
+     **/
+    @Test
+    public void test_commandType_given_addressCmd_when_getCmd_then_returnCmd() {
+        String builder = "@32" + System.lineSeparator() + "@label";
+        ByteArrayInputStream ins = new ByteArrayInputStream(builder.getBytes());
+        ParserImpl parser = new ParserImpl(ins);
+        parser.advance();
+        CommandType cmd = parser.commandType();
+        Assert.assertEquals(CommandType.ADDRESS_CMD, cmd);
+        parser.advance();
+        cmd = parser.commandType();
+        Assert.assertEquals(CommandType.ADDRESS_CMD, cmd);
+    }
+
+    /**
+     * @provided the control cmd and advance invoked
+     * @expected the control cmd type
+     **/
+    @Test
+    public void test_commandType_given_ctlCmd_when_getCmd_then_returnCtlCmd() {
+        List<String> cmds = Arrays.asList("M=A", "M=D-1", "D-1;M", "M=b;C", "b;c");
+        String str = String.join(System.lineSeparator(), cmds);
+        ByteArrayInputStream ins = new ByteArrayInputStream(str.getBytes());
+        ParserImpl parser = new ParserImpl(ins);
+
+        for (String ignored : cmds) {
+            parser.advance();
+            Assert.assertEquals(CommandType.CONTROL_CMD, parser.commandType());
+        }
+    }
+
+    /**
+     * @provided then location cmd and advance invoked
+     * @expected the location cmd
+     **/
+    @Test
+    public void test_commandType_given_lCommand_when_getCmd_then_returnCmd() {
+        List<String> cmds = Arrays.asList("(end)", "(LOOP)");
+        String str = String.join(System.lineSeparator(), cmds);
+        ByteArrayInputStream ins = new ByteArrayInputStream(str.getBytes());
+        ParserImpl parser = new ParserImpl(ins);
+
+        for (String ignored : cmds) {
+            parser.advance();
+            Assert.assertEquals(CommandType.LOCATION_CMD, parser.commandType());
+        }
+    }
+
+    /**
+     * @provided the address command
+     * @expected the cmd content
+     **/
+    @Test
+    public void test_symbol_given_addressCmd_when_getSymbol_then_returnTheStringSymbol() {
+        String builder = "@32" + System.lineSeparator() + "@label";
+        ByteArrayInputStream ins = new ByteArrayInputStream(builder.getBytes());
+        ParserImpl parser = new ParserImpl(ins);
+        parser.advance();
+        Assert.assertEquals("32", parser.symbol());
+        parser.advance();
+        Assert.assertEquals("label", parser.symbol());
+    }
+
+    /**
+     * @provided the location cmd
+     * @expected the cmd content
+     **/
+    @Test
+    public void test_symbol_given_locationCmd_when_getSymbol_then_returnStringSymbol() {
+        List<String> cmds = Arrays.asList("(end)", "(LOOP)");
+        String str = String.join(System.lineSeparator(), cmds);
+        ByteArrayInputStream ins = new ByteArrayInputStream(str.getBytes());
+        ParserImpl parser = new ParserImpl(ins);
+
+        parser.advance();
+        Assert.assertEquals("end", parser.symbol());
+        parser.advance();
+        Assert.assertEquals("LOOP", parser.symbol());
+    }
+
+    /**
+     * @provided the address cmd
+     * @expected the dest part or null
+     **/
+    @Test
+    public void test_dest_given_ctlCmd_when_getDest_then_returnDest() {
+        Map<String, String> map = new HashMap<>();
+        map.put("cmd=a", "null");
+        map.put("M=A", "M");
+        map.put("D=A", "D");
+        map.put("MD=A", "MD");
+        map.put("A=M", "A");
+        map.put("AM=M", "AM");
+        map.put("AD=M", "AD");
+        map.put("AMD=M", "AMD");
+
+        String str = map.keySet().stream().collect(Collectors.joining(System.lineSeparator()));
+        ByteArrayInputStream ins = new ByteArrayInputStream(str.getBytes());
+        ParserImpl parser = new ParserImpl(ins);
+
+        for (String expected : map.values()) {
+            parser.advance();
+            Assert.assertEquals(expected, parser.dest());
+        }
+    }
+
+    /**
+     * @provided control cmd and advance invoked
+     * @expected the comp content
+     **/
+    @Test
+    public void test_comp_given_ctlCmd_when_getComp_then_parseCorrectDest() {
+        Map<String, String> map = new HashMap<>();
+        map.put("M=0", "0");
+        map.put("M=1", "1");
+        map.put("M=-1", "-1");
+        map.put("M=D", "D");
+        map.put("M=A", "A");
+        map.put("M=!D", "!D");
+        map.put("M=!A", "!A");
+        map.put("M=-D", "-D");
+        map.put("M=-A", "-A");
+        map.put("M=D+1", "D+1");
+        map.put("M=A+1", "A+1");
+        map.put("M=D-1", "D-1");
+        map.put("D-1;A", "D-1");
+        map.put("M=A-1", "A-1");
+        map.put("M=D+A;M", "D+A");
+        map.put("M=D-A;A", "D-A");
+        map.put("M=A-D;M", "A-D");
+        map.put("D&A;A", "D&A");
+        map.put("M=D|A;M", "D|A");
+        map.put("M=M;D", "M");
+        map.put("M=!M", "!M");
+        map.put("M=-M", "-M");
+        map.put("M=M+1", "M+1");
+        map.put("M=M-1", "M-1");
+        map.put("M=D+M", "D+M");
+        map.put("M=D-M", "D-M");
+        map.put("M=M-D", "M-D");
+        map.put("M=D&M;M", "D&M");
+        map.put("D|M;A", "D|M");
+
+        String str = map.keySet().stream().collect(Collectors.joining(System.lineSeparator()));
+        ByteArrayInputStream ins = new ByteArrayInputStream(str.getBytes());
+        ParserImpl parser = new ParserImpl(ins);
+        for (String expected : map.values()) {
+            parser.advance();
+            Assert.assertEquals(expected, parser.comp());
+        }
+    }
+
+    /**
+     * @provided the control cmd
+     * @expected the jump symbol
+     **/
+    @Test
+    public void test_jump_given_ctlCmd_when_getJump_then_parseJump() {
+        Map<String, String> map = new HashMap<>();
+        map.put("M=0", "null");
+        map.put("M=0;Jxx", "null");
+
+        map.put("M=0;JGT", "JGT");
+        map.put("M=D&M;JGT", "JGT");
+        map.put("M;JGT", "JGT");
+
+        map.put("M=0;JEQ", "JEQ");
+        map.put("M=D&M;JEQ", "JEQ");
+        map.put("M;JEQ", "JEQ");
+
+        map.put("M=A;JGE", "JGE");
+        map.put("M=D+1;JGE", "JGE");
+        map.put("M;JGE", "JGE");
+
+        map.put("M=A;JLT", "JLT");
+        map.put("M=D+1;JLT", "JLT");
+        map.put("M;JLT", "JLT");
+
+        map.put("M=A;JNE", "JNE");
+        map.put("M=D+1;JNE", "JNE");
+        map.put("M;JNE", "JNE");
+
+        map.put("M=A;JLE", "JLE");
+        map.put("M=D+1;JLE", "JLE");
+        map.put("M;JLE", "JLE");
+
+        map.put("M=A;JMP", "JMP");
+        map.put("M=D+1;JMP", "JMP");
+        map.put("M;JMP", "JMP");
+
+        String str = map.keySet().stream().collect(Collectors.joining(System.lineSeparator()));
+        ByteArrayInputStream ins = new ByteArrayInputStream(str.getBytes());
+        ParserImpl parser = new ParserImpl(ins);
+        for (String expected : map.values()) {
+            parser.advance();
+            Assert.assertEquals(expected, parser.jump());
+        }
     }
 }
